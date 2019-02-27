9a401d0f62c3c88c177d18f5bbce6736b4afc440
alexh <git@alexh.name>
2019-02-27 13:29:42 +0100

    web_chart/cgi.sh: fix behavior for log files with few lines

--- web_chart/cgi.sh
+++ web_chart/cgi.sh
@@ -3,6 +3,7 @@
 set -eu
 
 logfile='/home/pi/src/bsec_bme680_linux/log.csv'
+n_logfile=0
 n_readings="${QUERY_STRING}"
 max_readings=100
 m=0
@@ -11,6 +12,20 @@ out=''
 # check if GET is int
 printf "%s\n" "${n_readings}" \
 | egrep -q '^[0-9]+$' >/dev/null
+
+# Check how many readings log file has.
+# Override n_readings if it's too large.
+# Take off 1 for first line in a CSV.
+n_logfile=$(
+  wc -l "${logfile}" | cut -d ' ' -f 1
+)
+n_logfile=$((
+  ${n_logfile} - 1
+))
+
+if [ "${n_readings}" -gt "${n_logfile}" ]; then
+  n_readings=${n_logfile}
+fi
 
 # Greeting
 printf "%s\n" 'Content-Type: text/plain; charset=utf-8'
